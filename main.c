4bdc5ec5d423fa2e70011ad3b0cb788b1ad11846
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-12-26 11:36:19 +0900

    Add branch by macOS

--- main.c
+++ main.c
@@ -16,10 +16,15 @@ int main(int argc, char** argv) {
   fprintf(stderr, "%d", node->ty);
 
   // Output the first part of the assembly. 
-  // (for macOS (if use linux, change _main to main))
   printf(".intel_syntax noprefix\n");
+
+#ifdef __APPLE__
   printf(".global _main\n");
   printf("_main:\n");
+#else
+  printf(".global main\n");
+  printf("main:\n");
+#endif
 
   // 抽象構文木を下りながらコード生成
   gen_x86(node);
